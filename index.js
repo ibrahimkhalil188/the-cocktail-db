60c1768ccca7b9a145b1d96c4caaccd351c066cc
unknown <ibrahimkhalil.mine@gmail.com>
2022-02-28 09:41:30 +0600

    menu

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const cocktailData = () => {
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    const inputField = document.getElementById("input-field").value
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputField}`)
         .then(res => res.json())
-        .then(data => displayCocktails(data.drinks[0]))
+        .then(data => showCocktailData(data.drinks))
 }
-
-cocktailData()
-
-const displayCocktails = cocktail => {
-    console.log(cocktail)
-    document.getElementById("body").style.backgroundImage = `url(${cocktail.strDrinkThumb})`
-
+//margarita
+//strTags
+const showCocktailData = cocktails => {
+    const displayCocktail = document.getElementById("displayCocktail")
+    displayCocktail.textContent = ""
+    cocktails.forEach(cocktail => {
+        console.log(cocktail)
+        const inputField = document.getElementById("input-field").value
+        if (inputField == '') {
+            document.getElementById("null-cocktail").style.display = "block"
+        }
+        const div = document.createElement("div")
+        div.innerHTML = `
+            <div class="col">
+                <div class="card">
+                    <img src="${cocktail.strDrinkThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${cocktail.strDrink}</h5>
+                        <p class="card-text">${cocktail.strInstructions}</p>
+                    </div>
+                </div>
+            </div>
+        `
+        displayCocktail.appendChild(div)
+    });
 }
